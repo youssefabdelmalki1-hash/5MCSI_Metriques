c29af5bd1dbe3672868869851453c87360727245
youssefabdelmalki1-hash <youssefabdelmalki1@gmail.com>
2025-12-02 12:24:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,5 +35,11 @@ from flask import render_template
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+-
+-
+
 if __name__ == "__main__":
   app.run(debug=True)
