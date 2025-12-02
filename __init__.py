b22e9f34ea85a144e6af7c651dad67de1f835dab
youssefabdelmalki1-hash <youssefabdelmalki1@gmail.com>
2025-12-02 12:05:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,20 @@ def hello_world():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin → °C
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    
+    return jsonify(results=results)
   
 if __name__ == "__main__":
   app.run(debug=True)
